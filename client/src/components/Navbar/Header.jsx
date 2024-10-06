2973c81de28064c3b1f2d9be3243a94a516c7f02
PR45H <Prashantpillai7@gmail.com>
2024-10-06 23:20:16 +0530

    Add Header component

---
+++ client/src/components/Navbar/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <div className='bg-[#E42529] flex justify-end text-white text-[14px] font-semibold'>
+                    <ul className='flex gap-4 py-1'>
+                        <NavLink to='/'>Find a store</NavLink>
+                        <div className='border-r border-opacity-70 border-white'></div>
+                        <NavLink to='/'>Buying guides</NavLink>
+                        <div className='border-r border-opacity-70 border-white'></div>
+                        <NavLink to='/'>Contact us</NavLink>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
