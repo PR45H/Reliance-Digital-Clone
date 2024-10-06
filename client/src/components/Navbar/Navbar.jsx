6f1b76dc0c84650cd8b74c7f4214690e6f02ed8e
PR45H <Prashantpillai7@gmail.com>
2024-10-06 23:20:23 +0530

    Add Navbar component

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Navbar = () => {
+    return (
+        <div>
+            <Header/>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
