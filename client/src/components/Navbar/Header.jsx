9a2e77e1ef6d9ab2e5e7ebfa1c026daa6d9c9bd4
PR45H <Prashantpillai7@gmail.com>
2024-10-07 01:00:24 +0530

    Refactor Header component styles for better alignment and spacing

--- client/src/components/Navbar/Header.jsx
+++ client/src/components/Navbar/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <div>
             <div>
-                <div className='bg-[#E42529] flex justify-end text-white text-[14px] font-semibold'>
-                    <ul className='flex gap-4 py-1'>
+                <div className='bg-[#E42529] flex justify-end text-white text-[14px] font-semibold px-4 py-2'>
+                    <ul className='flex gap-4'>
                         <NavLink to='/'>Find a store</NavLink>
                         <div className='border-r border-opacity-70 border-white'></div>
                         <NavLink to='/'>Buying guides</NavLink>
