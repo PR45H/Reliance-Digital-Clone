ff2169a45a1707f97ef4b2a36adcc49065b6d5f2
PR45H <Prashantpillai7@gmail.com>
2024-10-07 01:44:56 +0530

    Add Dusshera banner and CarouselCard component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import dussherBanner from '../assets/banners/Dusshera-HPMC-Banner-D-rev-3.avif'
+import CarouselCard from '../components/CarouselCard/CarouselCard'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <div className="welcomeBanner">
+                <img src={dussherBanner} alt="Dusshera Banner" className='w-full h-full object-cover' />
+            </div>
+            <div className="carouselBanner">
+                <CarouselCard />
+            </div>
+        </div>
     )
 }
 
