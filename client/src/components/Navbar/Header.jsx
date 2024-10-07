9213e99a6138f58131444a1a2547cde5b91b24bb
PR45H <Prashantpillai7@gmail.com>
2024-10-07 23:46:46 +0530

    Refactor Header component to dynamically render navigation links

--- client/src/components/Navbar/Header.jsx
+++ client/src/components/Navbar/Header.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({ content }) => {
     return (
-        <div>
-            <div>
-                <div className='bg-[#E42529] flex justify-end text-white text-[14px] font-semibold px-4 py-2'>
-                    <ul className='flex gap-4'>
-                        <NavLink to='/'>Find a store</NavLink>
-                        <div className='border-r border-opacity-70 border-white'></div>
-                        <NavLink to='/'>Buying guides</NavLink>
-                        <div className='border-r border-opacity-70 border-white'></div>
-                        <NavLink to='/'>Contact us</NavLink>
-                    </ul>
-                </div>
-            </div>
+        <div className='flex justify-end text-white text-[14px] font-semibold px-4 py-2'>
+            <ul className='flex gap-4'>
+                {content.map((item, index) => (
+                    <React.Fragment key={item.path}>
+                        <NavLink to={item.path}>{item.label}</NavLink>
+                        {index < content.length - 1 && (
+                            <div className='border-r border-opacity-70 border-white'></div>
+                        )}
+                    </React.Fragment>
+                ))}
+            </ul>
         </div>
     )
 }
