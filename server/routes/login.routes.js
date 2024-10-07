d56d89cc888f3074ce41e7c51eb1447184cc1721
PR45H <Prashantpillai7@gmail.com>
2024-10-08 02:29:59 +0530

    Add register route for user registration

--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -1,7 +1,9 @@
 const login = require('../controllers/User/login.controller');
+const register = require('../controllers/User/register.controller');
 
 const userRouter = require('express').Router();
 
 userRouter.post('/login', login);
+userRouter.post('/register', register);
 
 module.exports = userRouter
