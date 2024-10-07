5bd6e35a2734d7cfc80c352d73d0a2f3afbed82c
PR45H <Prashantpillai7@gmail.com>
2024-10-08 02:15:41 +0530

    Add login controller for user authentication

---
+++ server/controllers/User/login.controller.js
@@ -0,0 +1,23 @@
+const UserModel = require("../../models/user.model")
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+const privateKey = process.env.JWT_SECRET_KEY
+
+const login = async (req,res) => {
+    try {
+        const {email} = req.body
+        // console.log(email)
+        // Checking if user exist
+        const isEmailCorrect = await UserModel.findOne({ email: email })
+        if (!isEmailCorrect) {
+            return res.status(404).json({message:'User does not exist!'})
+        }
+        // Token creation and sending userId in the token
+        const token = jwt.sign({ userId: isEmailCorrect._id }, privateKey)
+        res.status(200).json({message: 'Login Successfull!', token:token})
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+module.exports = login
