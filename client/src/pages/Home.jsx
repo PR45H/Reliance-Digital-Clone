bac5203cff19aaa2b2abd9f2a7edc502bb516fe1
PR45H <Prashantpillai7@gmail.com>
2024-10-07 22:17:33 +0530

    Add corporate gift card banner to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import dussherBanner from '../assets/banners/Dusshera-HPMC-Banner-D-rev-3.avif'
+import corporateGiftCard from '../assets/banners/Corporate-Gift-Card-Strip-Banner.avif'
 import CarouselCard from '../components/CarouselCard/CarouselCard'
+import Banner from '@/components/Banner/Banner'
 
 const Home = () => {
     return (
         <div>
             <div className="welcomeBanner">
-                <img src={dussherBanner} alt="Dusshera Banner" className='w-full h-full object-cover' />
+                <Banner images={dussherBanner} />
             </div>
             <div className="carouselBanner">
                 <CarouselCard />
+            </div>
+
+            <div className="welcomeBanner">
+                <Banner images={corporateGiftCard} />
             </div>
         </div>
     )
