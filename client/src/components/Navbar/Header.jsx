62e6151e84c27a998e6b03a766ad3de4e6099a7e
PR45H <Prashantpillai7@gmail.com>
2024-10-08 00:01:19 +0530

    Refactor Header component to center navigation links and update hover style

--- client/src/components/Navbar/Header.jsx
+++ client/src/components/Navbar/Header.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 const Header = ({ content }) => {
     return (
-        <div className='flex justify-end text-white text-[14px] font-semibold px-4 py-2'>
+        <div className='bg-[#E42529] flex justify-center text-white text-[14px] font-semibold px-4 py-2'>
             <ul className='flex gap-4'>
                 {content.map((item, index) => (
-                    <React.Fragment key={item.path}>
-                        <NavLink to={item.path}>{item.label}</NavLink>
+                    <React.Fragment key={index}>
+                        <NavLink to={item.path} className='hover:text-gray-300'>{item.label}</NavLink>
                         {index < content.length - 1 && (
                             <div className='border-r border-opacity-70 border-white'></div>
                         )}
