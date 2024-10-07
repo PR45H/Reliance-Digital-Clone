2ae39107462b4ca65def9a519d67de0462d54ccf
PR45H <Prashantpillai7@gmail.com>
2024-10-08 00:01:14 +0530

    Refactor Login component to add Header with dynamic navigation links

--- client/src/components/Login-Signup/Login.jsx
+++ client/src/components/Login-Signup/Login.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import Header from '../Navbar/Header'
 
 const Login = () => {
+    const content = [{
+        label: "OUR BRAND PROMISE",
+        path: ""
+    }, {
+        label: "EASY RETURNS",
+        path: "/cancellation-and-return-policy"
+    }, {
+        label: "NEXT DAY DELIVERY",
+        path: ""
+    }, {
+        label: "SERVICE GUARANTEE",
+        path: ""
+    }, {
+        label: "UNMATCHED NETWORK",
+        path: ""
+    }, {
+        label: "Find a store",
+        path: "/locateus"
+    }, {
+        label: "Contact Us",
+        path: "/contactus"
+    }]
     return (
         <div>
-            
+            <div>
+                <Header content={content} />
+            </div>
         </div>
     )
 }
