e91b7ba87dc8eb91f18df94803f10c215390d2ad
PR45H <Prashantpillai7@gmail.com>
2024-10-07 23:46:56 +0530

    Refactor Home component to add Navbar and Header components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,10 +3,28 @@ import dussherBanner from '../assets/banners/Dusshera-HPMC-Banner-D-rev-3.avif'
 import corporateGiftCard from '../assets/banners/Corporate-Gift-Card-Strip-Banner.avif'
 import CarouselCard from '../components/CarouselCard/CarouselCard'
 import Banner from '@/components/Banner/Banner'
+import Navbar from '../components/Navbar/Navbar'
+import Header from '../components/Navbar/Header'
 
 const Home = () => {
+    const content = [{
+        label: "Find a Store",
+        path: "/find-a-store"
+    }, {
+        label: "Buying Guides",
+        path: "/buying-guides"
+    }, {
+        label: "Contact Us",
+        path: "/contact-us"
+    }]
     return (
         <div>
+            <div className='bg-[#E42529] py-2'>
+                <Header content={content} />
+            </div>
+            <div>
+                <Navbar/>
+            </div>
             <div className="welcomeBanner">
                 <Banner images={dussherBanner} />
             </div>
