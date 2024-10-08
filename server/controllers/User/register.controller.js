253b675e04dece8de710eb88d4454d1449526bbd
PR45H <Prashantpillai7@gmail.com>
2024-10-09 02:13:39 +0530

    Refactor register controller to convert firstName to lowercase before saving user

--- server/controllers/User/register.controller.js
+++ server/controllers/User/register.controller.js
@@ -2,14 +2,15 @@ const UserModel = require("../../models/user.model")
 
 const register = async (req,res) => {
     try {
-        const { firstName, lastName, email, mobile, password } = req.body
+        const { firstName, lastName, email, password, mobile } = req.body
+        const lowerCase  = firstName.toLowerCase()
         const isExistingUser = await UserModel.findOne({ email: email })
         if (isExistingUser) {
             return res.status(400).json({message:'User already exist!'})
         }
-        const newUser = new UserModel({firstName, lastName, email, mobile, password})
+        const newUser = new UserModel({firstName:lowerCase, lastName, email, mobile, password})
         await newUser.save()
-        res.status(200).json({message: 'User Registered!', user: newUser})
+        res.status(201).json({message: 'User Registered!', user: newUser})
     } catch (error) {
         res.status(500).json({error: error.message})
     }
