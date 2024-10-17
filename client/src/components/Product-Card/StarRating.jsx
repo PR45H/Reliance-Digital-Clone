c2c057acbbc02ae6a554b28064f0caf0fd91a3c4
PR45H <Prashantpillai7@gmail.com>
2024-10-17 23:32:02 +0530

    Add StarRating component to display product ratings

---
+++ client/src/components/Product-Card/StarRating.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const StarRating = ({ rating }) => {
+    const totalStars = 5;
+    const filledStars = Math.round(rating);
+    const emptyStars = totalStars - filledStars;
+
+    return (
+        <div className="text-xl">
+            {[...Array(filledStars)].map((_, index) => (
+                <span key={index} className="star text-[#FFA41C]">&#9733;</span>
+            ))}
+            {[...Array(emptyStars)].map((_, index) => (
+                <span key={index} className="star">&#9734;</span>
+            ))}
+        </div>
+    );
+}
+
+export default StarRating;
+
+// Add this CSS to your stylesheet or in a <style> tag
+{/* <style jsx>{`
+    .star-rating {
+        display: flex;
+        align-items: center;
+    }
+    .star {
+        font-size: 24px;
+        color: gold;
+        margin-right: 5px;
+    }
+`}</style> */}
