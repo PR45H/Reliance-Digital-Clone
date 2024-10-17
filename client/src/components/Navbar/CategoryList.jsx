1d7a6ce81979b05a9b37ac57c78edba33fdc8d81
PR45H <Prashantpillai7@gmail.com>
2024-10-17 23:32:30 +0530

    Refactor CategoryList component to improve navigation menu styling and structure

--- client/src/components/Navbar/CategoryList.jsx
+++ client/src/components/Navbar/CategoryList.jsx
@@ -13,32 +13,41 @@ const CategoryList = ({ product }) => {
         <div className="bg-[#003380] py-2 text-white text-sm font-semibold w-full">
             <NavigationMenu>
                 <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger className="bg-[#003380] hover:bg-[#E42529]">{product.map((category, index) => {
-                            return <div key={index}>{category.title}</div>
-                        })}</NavigationMenuTrigger>
-                        <NavigationMenuContent className="bg-[#003380] text-white">
+                    <NavigationMenuItem className="relative">
+                        <NavigationMenuTrigger className="bg-[#003380] hover:bg-[#E42529]">
+                            {product.map((category, index) => {
+                                return <div key={index}>{category.title}</div>;
+                            })}
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent className="bg-[#003380] text-white absolute left-1/2 transform -translate-x-1/2">
                             <NavigationMenuLink>
                                 {product.map((category, index) => {
-                                    return <div key={index} className="grid grid-cols-3 w-[1024px]">
-                                        {category.subCategories.map((subCategory, index) => {
-                                            return <div key={index} className="flex-col p-5 gap-3 bg-[#003380] text-white w-full ">
-                                                <h1 className="font-semibold text-[16px] mb-3">{subCategory.title}</h1>
-                                                <div className="text-[14px]">{subCategory.products.map((product,i) => {
-                                                    return <div key={i} className="mb-1 hover:text-gray-400 cursor-pointer">
-                                                            {product}
+                                    return (
+                                        <div key={index} className="grid grid-cols-3 w-[1024px]">
+                                            {category.subCategories.map((subCategory, index) => {
+                                                return (
+                                                    <div key={index} className="flex-col p-5 gap-3 bg-[#003380] text-white w-full ">
+                                                        <h1 className="font-semibold text-[16px] mb-3">{subCategory.title}</h1>
+                                                        <div className="text-[14px]">
+                                                            {subCategory.products.map((product, i) => {
+                                                                return (
+                                                                    <div key={i} className="mb-1 hover:text-gray-400 cursor-pointer">
+                                                                        {product}
+                                                                    </div>
+                                                                );
+                                                            })}
+                                                        </div>
                                                     </div>
-                                                })}</div>
-                                            </div>
-                                        })}
-                                    </div>
+                                                );
+                                            })}
+                                        </div>
+                                    );
                                 })}
                             </NavigationMenuLink>
                         </NavigationMenuContent>
                     </NavigationMenuItem>
                 </NavigationMenuList>
             </NavigationMenu>
-
         </div>
     );
 };
