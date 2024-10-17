bb1132d8c21a8308a8f3b4c79d2be820a89f93bf
PR45H <Prashantpillai7@gmail.com>
2024-10-18 01:01:14 +0530

    Refactor ProductCard component to include smartphone listing and improve styling

--- client/src/components/Product-Card/ProductCard.jsx
+++ client/src/components/Product-Card/ProductCard.jsx
@@ -10,34 +10,47 @@ import {
 import StarRating from './StarRating'
 
 
-const ProductCard = () => {
+const ProductCard = ({products}) => {
     return (
-        <div >
-            <div className="hover:shadow-xl rounded-xl border-2 w-[450px]">
+        <div className='grid grid-cols-4 gap-3'>
+            {products.smart_phones.map((product,index) => (
+                <div className="hover:shadow-xl cursor-pointer rounded-xl border-2 w-[420px]">
                 <div>
-                    <img src="https://www.reliancedigital.in/medias/OnePlus-Nord-CE-3-Lite-MOB-493665925-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNzUyMXxpbWFnZS9qcGVnfGltYWdlcy9oODMvaDU1Lzk5ODA3Mjk0NTg3MTguanBnfDkwNjYzNjhhODQxMzllYmNlZGMwYzQ0YjllNmEwZWQwNzIxNDk0OGFkOTFiZjU4MTU2N2FlMjg3YzFmMTBmZGQ" alt="oppo nord CE 3" className='p-5 w-56 transform transition-transform duration-500 hover:scale-110 m-auto'/>
+                    <img src={product.Image} alt={product.Products_name} className='p-5 w-56 transform transition-transform duration-500 hover:scale-110 m-auto'/>
                 </div>
-                <Card className="text-[#003380]" >
+                <Card className="text-[#003380] border-none" >
                     <CardHeader>
-                        <CardTitle className="font-normal">OnePlus Nord CE 3 Lite 5G 256 GB, 8 GB RAM, Pastel Lime, Mobile Phone</CardTitle>
+                        <CardTitle className="font-normal truncate-title">{product.Products_name}</CardTitle>
                         <CardDescription>
-                            <div className='flex gap-1'>
-                                <StarRating rating={4} />
-                                <span>(475)</span>
+                            <div className='flex gap-1 items-center'>
+                                <StarRating rating={product.Stars} />
+                                    <span>{ product.Ratings}</span>
                             </div>
                         </CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <p>Card Content</p>
+                        <div className='flex items-center gap-3 mb-3'>
+                                <p className='font-semibold'>&#8377;{product.New_Price}</p>
+                                <p className='line-through text-gray-700 font-medium text-sm opacity-80'>&#8377;{product.Old_Price}</p>
+                            <span className='text-[#49A047] text-sm font-medium' >{product.Discount}</span>
+                        </div>
+                        <div>
+                            <div className='w-fit bg-[#8cbd8b59] bg-opacity-20 border px-2 rounded-3xl border-[#49A047]'>
+                                <span className='text-[#49A047] font-normal text-sm'>10% UPTO RS. 1500 INST. DISC. WITH SBI DEBIT CARD</span>
+                            </div>
+                        </div>
                     </CardContent>
                     <CardFooter>
                         <p>Card Footer</p>
                     </CardFooter>
                 </Card>
 
-            </div>
+            </div>    
+            ))}
+            
         </div>
     )
 }
 
 export default ProductCard
+
