ecda869b839f0cf1f3ebc4b99be4c7ad7edeb4eb
PR45H <Prashantpillai7@gmail.com>
2024-11-21 22:53:26 +0530

    Refactor ProductCard component to enhance structure and improve styling

--- client/src/components/Product-Card/ProductCard.jsx
+++ client/src/components/Product-Card/ProductCard.jsx
@@ -14,11 +14,12 @@ const ProductCard = ({products}) => {
     return (
         <div className='grid grid-cols-4 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             {products.smart_phones.map((product,index) => (
-                <div className="hover:shadow-xl cursor-pointer rounded-xl border-2 w-full">
-                <div>
-                    <img src={product.Images} alt={product.Products_name} className='p-5 w-56 transform transition-transform duration-500 hover:scale-110 m-auto'/>
-                </div>
-                <Card className="text-[#003380] border-none" >
+                <div className="hover:shadow-xl cursor-pointer rounded-xl w-full">
+
+                    <Card className="text-[#003380] border-none" >
+                        <div>
+                            <img src={product.Images} alt={product.Products_name} className='p-5 w-56 transform transition-transform duration-500 hover:scale-110 m-auto'/>
+                        </div>
                     <CardHeader>
                         <CardTitle className="font-normal truncate">{product.Products_name}</CardTitle>
                         <CardDescription>
@@ -31,19 +32,19 @@ const ProductCard = ({products}) => {
                     <CardContent>
                         <div className='flex items-center gap-3 mb-3'>
                                 <p className='font-semibold'>&#8377;{product.New_Price}</p>
-                                <p className='line-through text-gray-700 font-medium text-sm opacity-80'>&#8377;{product.Old_Price}</p>
+                                <p className='line-through text-gray-700 font-medium text-sm opacity-80'>{product.Old_Price}</p>
                             <span className='text-[#49A047] text-sm font-medium' >{product.Discounts}</span>
                         </div>
                             <div>
                                 {product.Bank_Offers && <div className='w-fit bg-[#8cbd8b59] bg-opacity-20 border px-4 py-1 rounded-3xl border-[#49A047]'>
-                                    <span className='text-[#49A047] font-normal text-[12px]'>{product.Bank_Offers}</span>
+                                    <span className='text-[#49A047] font-normal text-[12px] '>{product.Bank_Offers}</span>
                             </div>}
                             
                         </div>
                     </CardContent>
-                    <CardFooter>
+                    {/* <CardFooter>
                         <p>Card Footer</p>
-                    </CardFooter>
+                    </CardFooter> */}
                 </Card>
 
             </div>    
