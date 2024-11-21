35bdb462edb054997e38bccd686d96ca8c6e08a9
PR45H <Prashantpillai7@gmail.com>
2024-11-21 22:53:44 +0530

    Add new categories to BottomNav component for improved navigation

--- client/src/components/Navbar/BottomNav.jsx
+++ client/src/components/Navbar/BottomNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CategoryList from './categoryList'
-import {mobiles_tablets, television, audio, home_appliances, computers} from './navbarCategoriesData'
+import {mobiles_tablets, television, audio, home_appliances, computers, cameras, kitchen_appliances, personal_care, accessories} from './navbarCategoriesData'
 
 
 const BottomNav = () => {
@@ -18,13 +18,17 @@ const BottomNav = () => {
     return (
         <div className=' '>
             {/* {console.log(mobiles_tablets)} */}
-            <div className='grid grid-cols-9'>
+            <div className='grid grid-flow-col'>
                 <CategoryList product={mobiles_tablets} />
                 <CategoryList product={television} />
                 <CategoryList product={audio} />
                 <CategoryList product={home_appliances} />
                 <CategoryList product={computers} />
-                <CategoryList product={computers} />
+                <CategoryList product={cameras} />
+                <CategoryList product={kitchen_appliances} />
+                <CategoryList product={personal_care} />
+                <CategoryList product={accessories} />
+                
             </div>
             
         </div>
