bac5cf7edbeb6ac56aa308be09bd8f26ccef1746
PR45H <Prashantpillai7@gmail.com>
2024-11-27 20:28:17 +0530

    Add Redux provider and Product route to main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,9 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import Home from './pages/Home.jsx'
 import Login from './components/Login-Signup/Login.jsx'
 import Signup from './components/Login-Signup/Signup.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store/reduxStore.js'
+import Product from './pages/Product.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -13,12 +16,14 @@ const router = createBrowserRouter(
       <Route path='' element={<Home/>}/>
       <Route path='login' element={<Login/>}/>
       <Route path='register' element={<Signup/>}/>
+      <Route path='product' element={<Product/>}/>
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </Provider>
+  
 )
