8b52084657598114642b6d4bf9083c07074fb42e
PR45H <Prashantpillai7@gmail.com>
2024-11-27 20:30:03 +0530

    Add product routes for handling product-related API requests

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const port = process.env.PORT;
 const connectDB = require('./config/dbConnection');
 const userRouter = require('./routes/login.routes');
-const cors = require('cors')
+const cors = require('cors');
+const productRouter = require('./routes/product.routes');
 
 // middleware
 app.use(express.json())
@@ -12,6 +13,7 @@ app.use(cors())
 
 // user routes
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.listen(port, () => {
     try {
